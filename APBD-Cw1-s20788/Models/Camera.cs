8927023faf1b37d43f11819e4ac0cc6c250c868a
sitv123@gmail.com <sitv123@gmail.com>
2026-03-28 22:40:45 +0100

    Utworzenie klas Camera oraz Projector

---
+++ APBD-Cw1-s20788/Models/Camera.cs
@@ -0,0 +1,8 @@
+﻿namespace APBD_Cw1_s20788.Models;
+
+public class Camera(int id, string name, EquipmentStatus status, bool isMirrorless, int megapixels): Item(id, name, status)
+{
+    public bool IsMirrorless { get; set; } = isMirrorless;
+    public int Megapixels { get; set; } = megapixels;
+    
+}
---
+++ APBD-Cw1-s20788/Models/Projector.cs
@@ -0,0 +1,8 @@
+﻿namespace APBD_Cw1_s20788.Models;
+
+public class Projector(int id, string name, EquipmentStatus status, bool is3dCompatible, int lumens): Item(id, name, status)
+{
+    public bool Is3dCompatible { get; set; } = is3dCompatible;
+    public int Lumens { get; set; } = lumens;
+    
+}
