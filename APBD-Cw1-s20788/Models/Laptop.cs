45ca43efa4a8210d85463d050aeaf2cab22480e8
sitv123@gmail.com <sitv123@gmail.com>
2026-03-28 22:41:33 +0100

    Dodanie dodatkowych własności dla klasy Laptop

--- APBD-Cw1-s20788/Models/Laptop.cs
+++ APBD-Cw1-s20788/Models/Laptop.cs
@@ -1,6 +1,8 @@
 ﻿namespace APBD_Cw1_s20788.Models;
 
-public class Laptop(int id, string name, EquipmentStatus status, ): Item(id, name, status)
+public class Laptop(int id, string name, EquipmentStatus status, bool hasDedicatedGpu, int gpuMemory): Item(id, name, status)
 {
+    public bool HasDedicatedGpu { get; set; } = hasDedicatedGpu;
+    public int GpuMemory { get; set; } = gpuMemory;
     
 }
