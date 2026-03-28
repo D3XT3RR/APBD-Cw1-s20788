ade2d67a0f79e56257e3f6ba66ede2473bf8be59
sitv123@gmail.com <sitv123@gmail.com>
2026-03-28 22:43:20 +0100

    Utworzenie repozytorium urządzeń

---
+++ APBD-Cw1-s20788/Repositories/EquipmentRepository.cs
@@ -0,0 +1,23 @@
+﻿using APBD_Cw1_s20788.Exceptions;
+using APBD_Cw1_s20788.Models;
+
+namespace APBD_Cw1_s20788.Repositories;
+
+public class EquipmentRepository
+{
+    private readonly List<Item> _items = [];
+    
+    public void Add(Item item)
+    {
+        _items.Add(item);
+    }
+    public IEnumerable<Item> GetAll()
+    {
+        return _items.AsReadOnly();
+    }
+    public Item Get(int id)
+    {
+        var item = _items.Find(e => e.Id == id);
+        return item ?? throw new ItemNotFoundException(id);
+    }
+}
