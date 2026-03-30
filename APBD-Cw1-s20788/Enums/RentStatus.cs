a37cde1ec675545a27389004ed6e7584a8214f91
sitv123@gmail.com <sitv123@gmail.com>
2026-03-30 18:52:44 +0200

    Utworzenie klas wypożyczenia oraz statusu wypożyczenia

---
+++ APBD-Cw1-s20788/Enums/RentStatus.cs
@@ -0,0 +1,6 @@
+﻿namespace APBD_Cw1_s20788.Enums;
+
+public enum RentStatus
+{
+    Active, Returned
+}
---
+++ APBD-Cw1-s20788/Models/Rental.cs
@@ -0,0 +1,15 @@
+﻿using APBD_Cw1_s20788.Enums;
+
+namespace APBD_Cw1_s20788.Models;
+
+public class Rental(int id, DateTime startDate, User user, Item item): Entity(id)
+{
+    
+    public User User { get; set; } = user;
+    public Item Item { get; set; } = item;
+    public DateTime StartDate { get; set; } = startDate;
+    public DateTime? ReturnDate { get; set; }
+    
+    public RentStatus Status => ReturnDate is null ? RentStatus.Returned : RentStatus.Active;
+    
+}
